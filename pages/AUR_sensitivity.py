a00870954de3f780bfc3d6de9599e10cde497d40
Tumas Rackaitis <tumasrackaitis@Tumass-MacBook-Pro.local>
2022-09-28 15:04:31 -0500

    revamped montecarlo app

--- pages/AUR_sensitivity.py
+++ pages/AUR_sensitivity.py
@@ -10,13 +10,21 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def plot_distribution(dist_variates, title=""):
-    dist_variates = dist_variates/dist_variates.sum()
+def plot_distribution(dist_variates, title="",secondary_y=None):
+    #dist_variates = dist_variates/dist_variates.sum()
     fig = plt.figure(figsize=(10, 4))
-    sns.distplot(dist_variates, bins=100,norm_hist=True)
-    fig.suptitle(title, fontsize=16)
-    return st.pyplot(fig)
+    if secondary_y is None:
 
+        sns.distplot(dist_variates, bins=100,norm_hist=True)
+
+        fig.suptitle(title, fontsize=16)
+        return st.pyplot(fig)
+    else:
+        for var in [dist_variates,secondary_y]:
+            sns.distplot(var, bins=100,norm_hist=True)
+
+        fig.suptitle(title, fontsize=16)    
+        return st.pyplot(fig)
 
 sns.set_style('darkgrid')
 num_variates = 5000
@@ -31,212 +39,71 @@ st.markdown("""
     All distirbutions have fat tails and excess kurtosis, and only  in some special choices or parameters resemble normal distributions.   
     """)
 st.header('The Opportunity')
-st.markdown(
-    "The Aurora driver is priced $0.50 per mile, equal to trucking labor. Long haul truck avg speed is 60 mph. It's a question of utilization.  ")
-opp_twenty_four, opp_twenty_five, opp_twenty_six = st.tabs(
-    ["2024 ARR build", "2025 ARR build", "2026 ARR build"])
 
-with opp_twenty_four:
-    st.markdown("By 2024, Aurora plans to fully launch the Aurora Driver, with about ~100-250 trucks. Trucks and utilization are distributed as follows:")
-    with st.expander("Distributions for 2024:"):
-    # distribution of trucks
+opp_truck,opp_rates,opp_util = st.tabs(["#Trucks build","rate build",'utilization build'])
+
+with opp_truck:
+    with st.expander("How many Trucks will AUR power?:"):
         twenty_four_truck_mean = st.slider(
-            "Expected amount of trucks in 2024", min_value=50, max_value=400, value=100)
-        twenty_four_truck_dist = sp.t(
+            "Expected amount of trucks", min_value=50, max_value=10000, value=1000)
+        truck_dist = sp.t(
             100, loc=twenty_four_truck_mean).rvs(num_variates)
-        plot_distribution(twenty_four_truck_dist,
-                        f"Distribution of Trucks in 2024")
-        # distribution of loads
-        st.markdown(
-            'By 2024, Aurora plans to be ~20% utilized. Truckers maximizing their ELD time are on the road 35\% of the year. ')
-        twenty_four_loads_alpha = st.slider(
-            'Positive bias on how many hours/year Aurora drivers are on the road in 2024 ', min_value=1, max_value=20, value=3)
-        twenty_four_loads_beta = st.slider(
-            'Negative bias on how many hours/year Aurora drivers are on the road in 2024 ', min_value=1, max_value=20, value=6)
-        twenty_four_loads_var = st.slider(
-            "Dispersion on how many hours/year Aurora drivers are on the road in 2024", min_value=0.01, max_value=0.2, value=0.1)
-        twenty_four_util_dist = sp.beta(
-            twenty_four_loads_alpha, twenty_four_loads_beta, loc=0.2, scale=twenty_four_loads_var).rvs(num_variates)
-        plot_distribution(twenty_four_util_dist,
-                        f"Distribution of Loads in 2024 (thousands), Mean: {round(twenty_four_util_dist.mean(),2)}")
-        # distribution of revenue
-        st.markdown(
-            "This results in the following expected distribution of revenue for AUR in 2024: ")
-        twenty_four_revenue = ( twenty_four_util_dist*365*24 )* (60*0.5)* twenty_four_truck_dist # % truck utilization * hours in year * 60 mph avg at 50c * trucks 
+        plot_distribution(truck_dist,
+                        f"Distribution of Trucks")
+        lease_to_own_pct = st.slider(
+            "How many trucks are lease-to-own vs full price?", min_value=0.0, max_value=1.0, value=0.8)
+        #price of lease to own
+        lease_to_own_price = 20000# 20k 
+        truck_own_price = 180000#180k
+        fleet_cost = (lease_to_own_pct * truck_dist *lease_to_own_price  +  truck_dist * (1-lease_to_own_pct) * truck_own_price )*10**-6
+        plot_distribution(fleet_cost,f"Distribution of fleet cost for AUR ($mns) , Mean:{ round(fleet_cost.mean(),2)}")
 
-        plot_distribution(twenty_four_revenue,
-                        f"Distribution of AUR 2024 ARR($mns), mean:{round(twenty_four_revenue.mean(),2)}")
-
-with opp_twenty_five:
-    st.markdown(
-        "By 2025, Aurora plans to fully launch the Aurora Driver, with about ~250 trucks. Trucks are distributed as below:")
-    # distribution of trucks
-    with st.expander("Distributions for 2025:"):
-
-        twenty_five_truck_mean = st.slider(
-            "Expected amount of trucks in 2025", min_value=50, max_value=1000, value=500)
-        twenty_five_truck_dist = sp.t(
-            100, loc=twenty_five_truck_mean).rvs(num_variates)
-        plot_distribution(twenty_five_truck_dist,
-                        f"Distribution of Trucks in 2025")
-        # distribution of loads
-        st.markdown(
-            'By 2025, Aurora plans to be 25\% utilized')
-        twenty_five_loads_alpha = st.slider(
-            'Positive bias on how many hours/year Aurora drivers are on the road in 2025 ', min_value=1, max_value=20, value=3)
-        twenty_five_loads_beta = st.slider(
-            'Negative bias on how many hours/year Aurora drivers are on the road  in 2025 ', min_value=1, max_value=20, value=3)
-        twenty_five_loads_var = st.slider(
-            "Dispersion on how many hours/year Aurora drivers are on the road in 2025", min_value=0.01, max_value=0.2, value=0.1)
-        twenty_five_util_dist = sp.beta(
-            twenty_five_loads_alpha, twenty_five_loads_beta, loc=.2, scale=twenty_five_loads_var).rvs(num_variates)
-        plot_distribution(twenty_five_util_dist,
-                        f"Distribution of truck utilization in 2025 (thousands), Mean: {round(twenty_five_util_dist.mean(),2)}")
-        # distribution of revenue
-        st.markdown(
-            "This results in the following expected distribution of revenue for AUR in 2025:")
-        twenty_five_revenue = ( twenty_five_util_dist*365*24 )* (60*0.5)* twenty_five_truck_dist *10**-6# % truck utilization * hours in year * 60 mph avg at 50c * trucks 
-        plot_distribution(twenty_five_revenue,
-                        f"Distribution of AUR 2025 ARR($mns), mean:{round(twenty_five_revenue.mean(),2)}")
-
-with opp_twenty_six:
-    with st.expander("Distributions for 2026:"):
-        st.markdown(
-            "By 2026, Aurora plans to fully launch the Aurora Driver, with about 1000 trucks. Trucks are distributed as below:")
-        # distribution of trucks
-        twenty_six_truck_mean = st.slider(
-            "Expected amount of trucks in 2026", min_value=50, max_value=3000, value=1550)
-        twenty_six_truck_dist = sp.t(
-            100, loc=twenty_six_truck_mean).rvs(num_variates)
-        plot_distribution(twenty_six_truck_dist, f"Distribution of Trucks in 2026")
-        # distribution of loads
-        st.markdown('By 2026, Aurora plans to mass market at ~40\% utilization. Average LTL load in southern US is 500 miles')
+with opp_rates:
+    with st.expander("How much does AUR get per mile / drive?"):
+        st.markdown('AUR driver drives the speed of traffic, capped at 65 mph. Avg LTL rate is 50c /mile, but is higher on some routes')
+        rates_alpha = st.slider(
+            'Positive bias of rate per mile (dollars)', min_value=1, max_value=20, value=2)
+        rates_beta = st.slider(
+            'Negative bias of rate per mile (dollars) ', min_value=1, max_value=20, value=7)
+        rate_var = st.slider(
+            "Dispersion of rate per mile (dollars)",  min_value=0.01, max_value=0.2, value=0.1)
+        rate_dist = sp.beta(rates_alpha, rates_beta,
+                                    loc=.47, scale=rate_var).rvs(num_variates)
+        plot_distribution(rate_dist,
+                        f"Distribution of rate per mile (dollars), Mean: {round(rate_dist.mean(),2)}")
+        mph_a = st.slider(
+            'Positive bias of avg mph', min_value=1, max_value=20, value=2)
+        mph_b = st.slider(
+            'Negative bias of avg mph ', min_value=1, max_value=20, value=18)
+        mph_var = st.slider(
+            "Dispersion of avg mph",  min_value=1, max_value=10, value=2)
+        mph_dist = sp.beta(mph_a, mph_b,
+                                    loc=58, scale=mph_var).rvs(num_variates)
+        plot_distribution(mph_dist,
+                        f"Distribution of ravg mph, Mean: {round(mph_dist.mean(),2)}")
+with opp_util:
+    with st.expander("How much will these trucks get utilized?"):
+        st.markdown('By 2026, Aurora plans to mass market at ~60\% utilization. Average LTL load in southern US is 500 miles')
         twenty_six_loads_alpha = st.slider(
-            'Positive bias on on how many hours/year Aurora drivers are on the road  in 2026 ', min_value=1, max_value=20, value=8)
+            'Positive bias on on how many hours/year Aurora drivers are on the road  ', min_value=1, max_value=20, value=8)
         twenty_six_loads_beta = st.slider(
-            'Negative bias on how many hours/year Aurora drivers are on the road  in 2026 ', min_value=1, max_value=20, value=3)
+            'Negative bias on how many hours/year Aurora drivers are on the road  ', min_value=1, max_value=20, value=3)
         twenty_six_loads_var = st.slider(
-            "Dispersion on how many hours/year Aurora drivers are on the road  in 2026",  min_value=0.01, max_value=0.2, value=0.1)
+            "Dispersion on how many hours/year Aurora drivers are on the road ",  min_value=0.01, max_value=0.2, value=0.1)
         twenty_six_util_dist = sp.beta(twenty_six_loads_alpha, twenty_six_loads_beta,
                                     loc=.4, scale=twenty_six_loads_var).rvs(num_variates)
         plot_distribution(twenty_six_util_dist,
-                        f"Distribution of Utilization in 2026 (thousands), Mean: {round(twenty_six_util_dist.mean(),2)}")
-        # distribution of revenue
-        st.markdown(
-            "This results in the following expected distribution of revenue for AUR in 2026:")
-        twenty_six_revenue = ( twenty_six_util_dist*365*24 )* (60*0.5)* twenty_six_truck_dist *10**-6# % truck utilization * hours in year * 60 mph avg at 50c * trucks 
+                        f"Distribution of Utilization, Mean: {round(twenty_six_util_dist.mean(),2)}")
 
-        plot_distribution(
-            twenty_six_revenue, f"Distribution of AUR 2026 ARR ($mns), mean:{round(twenty_six_revenue.mean(),2)}")
+st.markdown("All of these inputs imply the following distribution of revenue:")
+revenue_dist = twenty_six_util_dist* rate_dist * mph_dist*truck_dist * 365 *24 * 10**-6
+plot_distribution(revenue_dist, f"Distribution of annual revenue ($mns), mean: {round(revenue_dist.mean(),2)}")
 
-
-st.markdown("Taking the fully diluted market cap of AUR (~2.6 bn), we can take the 2026 Price to ARR multiple Distribution")
-
-plot_distribution((2.6*1000)/twenty_six_revenue,
-                  f'Price/2026 ARR multiple distribution: Mean:{round(np.mean((2.6*1000)/ twenty_six_revenue),2)}')
-
-st.header("The Cash Burn:")
-
-st.markdown("Aurora has about 1.378 bn in cash, burning about 210 mn a quarter. After attending their investor day, they plan to significantly ramp up their S&M spend and keep R&D flat. Here's some ways that can play out:")
-burn22, burn23, burn24, burn25, burn26 = st.tabs(
-    ['2022 burn', "2023 burn", "2024 burn", "2025 burn", "2026 burn"])
-with burn22:
-    with st.expander("Burn distributions for 2022:"):
-
-        st.markdown(
-            "R&D spend is about 180 mn in Q2. Modelling it out for the rest of the year: ")
-        rnd22 = st.slider("expected R&D burn ($mns) in 2H 2022",
-                        min_value=300, max_value=500, value=420)
-        rnd22_dist = sp.t(100, loc=rnd22).rvs(num_variates)
-        sm22 = st.slider("expected S&GA burn ($mns) in 2H 2022",
-                        min_value=40, max_value=80, value=62)
-        sm22_dist = sp.t(100, loc=sm22).rvs(num_variates)
-        st.markdown("This puts the opex burn in 2022 at:")
-        opex22_dist = rnd22_dist+sm22_dist
-        plot_distribution(opex22_dist, "Opex loss in 2022")
-with burn23:
-    with st.expander("Burn distributions for 2023:"):
-
-        st.markdown("R&D spend supposed to stay constant in 2023: ")
-        rnd23 = st.slider("expected R&D burn ($mns) in 2023",
-                        min_value=400, max_value=1000, value=720)
-        rnd23_dist = sp.t(100, loc=rnd23).rvs(num_variates)
-        sm23 = st.slider("expected S&GA burn ($mns) in 2023",
-                        min_value=120, max_value=160, value=130)
-        sm23_dist = sp.t(100, loc=sm23).rvs(num_variates)
-        st.markdown("This puts the opex burn in 2022 at:")
-        opex23_dist = rnd23_dist+rnd23_dist
-        plot_distribution(opex23_dist, "Opex loss in 2023")
-with burn24:
-    with st.expander("Burn distributions for 2024:"):
-
-        st.markdown(
-            "R&D spend likely gets cut in 2024, but with S&M rising as loads need to be met. ")
-        rnd24 = st.slider("expected R&D burn ($mns) in 2024",
-                        min_value=300, max_value=1000, value=700)
-        rnd24_dist = sp.t(100, loc=rnd24).rvs(num_variates)
-        sm24 = st.slider("expected S&GA burn ($mns) in 2024",
-                        min_value=100, max_value=250, value=160)
-        sm24_dist = sp.t(100, loc=sm24).rvs(num_variates)
-        st.markdown("This puts the opex burn in 2024 at:")
-        opex24_dist = rnd24_dist+sm24_dist
-        plot_distribution(opex24_dist, "Opex loss in 2024")
-
-with burn25:
-    with st.expander("Burn distributions for 2025:"):
-
-        st.markdown(
-            "If AUR is able to go to market, I  that they'll focus getting this out, and keeping expenses flat")
-        rnd25 = st.slider("expected R&D burn ($mns) in 2025",
-                        min_value=300, max_value=1000, value=700)
-        rnd25_dist = sp.t(100, loc=rnd25).rvs(num_variates)
-        sm25 = st.slider("expected S&GA burn ($mns) in 2025",
-                        min_value=100, max_value=250, value=160)
-        sm25_dist = sp.t(100, loc=sm25).rvs(num_variates)
-        st.markdown("This puts the opex burn in 2025 at:")
-        opex25_dist = rnd25_dist+sm25_dist
-        plot_distribution(opex25_dist, "Opex loss in 2025")
-
-with burn26:
-    with st.expander("Burn distributions for 2026"):
-        st.markdown(
-            "Similiarly with 2025, I wager that they'll focus getting this out, and keeping expenses flat")
-        rnd26 = st.slider("expected R&D burn ($mns) in 2026",
-                        min_value=300, max_value=1000, value=700)
-        rnd26_dist = sp.t(100, loc=rnd26).rvs(num_variates)
-        sm26 = st.slider("expected S&GA burn ($mns) in 2026",
-                        min_value=100, max_value=250, value=160)
-        sm26_dist = sp.t(100, loc=sm26).rvs(num_variates)
-        st.markdown("This puts the opex burn in 2026 at:")
-        opex26_dist = rnd26_dist+sm26_dist
-        plot_distribution(opex25_dist, "Opex loss in 2026")
-
-st.markdown(
-    "Summing the opex distributions together, we get the following expected total burn over 2026:"
-    )
-total_burn = opex22_dist + opex23_dist+opex24_dist + opex25_dist + opex26_dist
-with st.expander("Total cash spent in operating expenses by 2026:"):
-    plot_distribution(total_burn, f"Expected cash spent in operating expenses by 2026, mean: {round(total_burn.mean(),2)}")
-
-st.header("EBITDA Profitability:")
-st.markdown("Using what we've modelled, by 2025, can we expect to see EBITDA profitability? ")
-ebitda_2025 = twenty_five_revenue - opex25_dist
-with st.expander("2025  Adj EBITDA distribution: "):
-    plot_distribution(ebitda_2025,f"Expected EBITDA in 2025. Mean:{round(ebitda_2025.mean(),2)}")
-
-st.markdown("This puts the Price / EBITDA in 2026?")
-ebitda_2026 = twenty_six_revenue - opex26_dist
-price26_ebitda_dist =(2.6 * 10**3) / ebitda_2026
-plot_distribution(price26_ebitda_dist, f"Expected Price/ 2026 EBITDA. Mean: {round(ebitda_2026.mean(),2)}")
-
-
-st.header("Incorporating a catastrophe")
-st.markdown("Self driving trucking is hard, and it might not take much to get them banned off the roads if there's a catastrophe, where AUR makes no revenue for a year. ")
-
-catas_mean = st.slider("Mean Probability of a disaster per year",min_value=0.001,max_value=0.2,value=0.005 )
-prob_catas = (1 -catas_mean)**4 #2023,2024,2025,2026
-weighted_ARR_multiple =  ((2.6*1000) /(prob_catas *twenty_six_revenue))
-st.metric("Probability of no disaster in four years:", str(round(prob_catas,2)*100) + "%")
-plot_distribution( weighted_ARR_multiple, "Disaster adjusted 2026 P/ARR multiple")
-
+st.markdown('The distribution of years to pay off their trucks, with selected margin below is:')
+ebitda_margin = st.slider(
+            'AUR scale adj EBITDA margin ', min_value=0.0, max_value=1.0, value=.15)
+days_to_payoff = fleet_cost / (revenue_dist* ebitda_margin )
+humans_payoff_time = fleet_cost / (0.3* rate_dist * mph_dist*truck_dist * 365 *24 * 10**-6 * (ebitda_margin-0.04))
+st.markdown("Payoff time for the fleet compared to working with humans(orange) vs AI drivers (Blue):")
+plot_distribution(days_to_payoff,f'years to pay off fleet: mean: {round(days_to_payoff.mean(),2)}',humans_payoff_time)
 
